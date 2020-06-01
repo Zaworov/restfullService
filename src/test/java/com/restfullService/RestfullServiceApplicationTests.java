d3d79c373050ace9837d0c1edb0c8129bb8ec217
Zawodniak <Piotr.Zawodniak@thomsonreuters.com>
2020-06-02 00:12:31 +0200

    Initial commit

---
+++ src/test/java/com/restfullService/RestfullServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restfullService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
